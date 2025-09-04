d3f426bbbf409e7ba985e7dd63ce7badf5429933
Lucas Zimmer <cherubim@MacBookPro.fritz.box>
2025-09-04 10:45:09 +0200

    Add build

---
+++ build/CMakeFiles/blackhole.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blackhole.dir/blackhole.cpp.o"
+  "CMakeFiles/blackhole.dir/blackhole.cpp.o.d"
+  "blackhole"
+  "blackhole.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blackhole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
